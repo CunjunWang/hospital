1dc40255603a27e74c75e9e1b07332f1727c1d9b
Cunjun Wang <13621691063@163.com>
2018-11-25 13:23:31 +0800

    [Basic Integration] Integrated with Swagger2 and added test controller.

--- src/main/java/com/cunjunwang/hospital/HospitalApplication.java
+++ src/main/java/com/cunjunwang/hospital/HospitalApplication.java
@@ -2,6 +2,7 @@ package com.cunjunwang.hospital;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,10 +14,5 @@ import org.springframework.web.bind.annotation.RestController;
 public class HospitalApplication {
     public static void main(String[] args) {
         SpringApplication.run(HospitalApplication.class, args);
-    }
-
-    @GetMapping("/")
-    public String test() {
-        return "app started";
     }
 }
